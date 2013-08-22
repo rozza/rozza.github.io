ed865cc15dd809ef057e64fde2ae5952ec2daaba
Ross Lawley <ross.lawley@gmail.com>
2013-08-22 17:40:58 +0100

    Updated publish script

--- publish.sh
+++ publish.sh
@@ -17,7 +17,13 @@ if git diff-index --quiet HEAD --; then
     mv .public/* .
     rm -rf .public
 
+    echo " ======================== "
+    echo "            WIN          "
+    echo " ======================== "
     echo " Please check the updated site and checkin ..."
 else
+    echo " ======================== "
+    echo "            FAIL          "
+    echo " ======================== "
     echo "You have changes not checked-in - cannot automatically update master"
 fi
